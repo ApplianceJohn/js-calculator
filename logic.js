5cd5597a5654ef6962483e208308d1e340b53daa
ApplianceJohn <thednbgremlin@gmail.com>
2018-08-09 12:24:29 -0400

    add basic css styling and js math function

---
+++ logic.js
@@ -0,0 +1,14 @@
+function operate(a, b, op) {
+    switch (op) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            return a / b;
+    }
+}
+
+console.log(operate(5, 4, "/"));
