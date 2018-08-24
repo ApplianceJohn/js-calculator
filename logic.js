3588fb9bb51f87709799aba9be89ca1f5a01361e
ApplianceJohn <thednbgremlin@gmail.com>
2018-08-24 14:40:05 -0400

    calculator now functional, elements re-aligned

--- logic.js
+++ logic.js
@@ -1,14 +1,27 @@
-function operate(a, b, op) {
-    switch (op) {
-        case "+":
-            return a + b;
-        case "-":
-            return a - b;
-        case "*":
-            return a * b;
-        case "/":
-            return a / b;
+tally = 0;
+
+function operate() {
+    document.getElementById("expression").value =
+        eval(document.getElementById("expression").value);
+}
+
+function input(e) {
+    if (this.id !== "cle" && this.id !== "equ") {
+        document.getElementById("expression").value =
+            document.getElementById("expression").value + this.value;
+    } else if (this.id === "cle" &&
+        document.getElementById("expression").value !== '') {
+            document.getElementById("expression").value = '';
+    } else if (this.id === "cle") {
+        tally = 0;
+    } else if (this.id === "equ") {
+        operate();
     }
 }
 
-console.log(operate(5, 4, "/"));
+var buttons = document.getElementsByClassName("press");
+var operators = document.getElementsByClassName("op");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', input);
+}
